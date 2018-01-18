dbaf7a47ef46e465ab533d44cd7aa9df7e3bdb04
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-01-18 10:35:57 +0800

    have done

--- class_unggas.cpp
+++ class_unggas.cpp
@@ -31,59 +31,77 @@ int main(){
 	ayamlaki.jumlahTelur(0);
 	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri ayam bini: \n\n";
 	cout <<(ayambini.renang);
 	ayambini.suara("kutakkutak");
 	ayambini.terbang("kada kawa tinggi ");
 	ayambini.ukuranTelur("sadang");
 	ayambini.jumlahTelur(15);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri itik laki: \n\n";
 	cout <<(itiklaki.renang);
 	itiklaki.suara("gugakgugak");
 	itiklaki.terbang("harat jua ");
 	itiklaki.ukuranTelur("laki kada bahintalu");
 	itiklaki.jumlahTelur(0);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri itik bini: \n\n";
 	cout <<(itikbini.renang);
 	itikbini.suara("gakgakgak");
 	itikbini.terbang("harat jua ");
 	itikbini.ukuranTelur("ganal pada ayam");
 	itikbini.jumlahTelur(22);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri angsa laki: \n\n";
 	cout <<(angsalaki.renang);
 	angsalaki.suara("nyuukkk");
 	angsalaki.terbang("sadang haja ");
 	angsalaki.ukuranTelur("laki kada bahintalu");
 	angsalaki.jumlahTelur(0);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri angsa bini: \n\n";
 	cout <<(angsabini.renang);
 	angsabini.suara("nyaakkk");
 	angsabini.terbang("sadang haja ");
 	angsabini.ukuranTelur("ganal banar");
 	angsabini.jumlahTelur(10);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri puyuh laki: \n\n";
 	cout <<(puyuhlaki.renang);
 	puyuhlaki.suara("kurrrhhh");
 	puyuhlaki.terbang("kada bisa tarabang ");
 	puyuhlaki.ukuranTelur("laki kada bahintalu");
 	puyuhlaki.jumlahTelur(0);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri puyuh bini: \n\n";
 	cout <<(puyuhbini.renang);
 	puyuhbini.suara("wruuhhh");
 	puyuhbini.terbang("kada bisa ");
 	puyuhbini.ukuranTelur("halus");
 	puyuhbini.jumlahTelur(30);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri belibis laki: \n\n";
 	cout <<(belibislaki.renang);
 	belibislaki.suara("ckakckak");
 	belibislaki.terbang("tinggi banar ");
 	belibislaki.ukuranTelur("laki kada bahintalu");
 	belibislaki.jumlahTelur(0);
+	cout <<"_______________________________\n\n";
 	
+	cout <<"Ciri-ciri belibis bini: \n\n";
 	cout <<(belibisbini.renang);
 	belibisbini.suara("ckaaakkk");
 	belibisbini.terbang("tinggi banar ");
 	belibisbini.ukuranTelur("sadang");
 	belibisbini.jumlahTelur(6);
+	cout <<"_______________________________\n\n";
 }
 void unggas::suara(string bunyi){
 	cout <<"\nbunyinya "<<bunyi<<endl;
